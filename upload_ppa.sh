17b48b0bc2e99edd011aa0259fa636295146a0e5
Matthias <borim7@web.de>
2021-01-07 15:38:00 +0100

    Support multiple ubuntu series for ppa upload

--- upload_ppa.sh
+++ upload_ppa.sh
@@ -3,35 +3,39 @@
 set -e
 
 # ppa require ubuntu distribution series in first line of changelog
-UBUNTU_SERIES=focal
+UBUNTU_SERIES="focal groovy"
 # only if same release need to be updated again
 PPA_VERSION=
 
 SRC_ARCHIVE_PATH=dist/*.tar.bz2
 SRC_ARCHIVE=`basename $SRC_ARCHIVE_PATH`
-#PACKAGE_NAME=radiotray-0.8.0
 PACKAGE_NAME="${SRC_ARCHIVE%.tar.bz2}"
 
 rm -rf ppa/*
-mkdir -p ppa/$PACKAGE_NAME/debian
-tar -xmf $SRC_ARCHIVE_PATH -C ppa/
 
-for FILE in $(git ls-files debian)
+for SERIES in $UBUNTU_SERIES
 do
-	cp $FILE ppa/$PACKAGE_NAME/debian
+	BASE_DIR=ppa/$SERIES
+	PACKAGE_DIR=$BASE_DIR/$PACKAGE_NAME
+
+	mkdir -p $PACKAGE_DIR/debian
+	tar -xmf $SRC_ARCHIVE_PATH -C ppa/$SERIES/
+
+	for FILE in $(git ls-files debian)
+	do
+		cp $FILE $PACKAGE_DIR/debian
+	done
+
+	# update changelog for ppa
+	# replace debian series with ubuntu series
+	sed -i "1s/unstable;/$SERIES;/" $PACKAGE_DIR/debian/changelog
+	# update version
+	sed -i "1s/)/-ppa$PPA_VERSION~$SERIES)/" $PACKAGE_DIR/debian/changelog
+
+	# create source package
+	(cd $PACKAGE_DIR && debuild -S)
+
+	# upload source package
+	dput ppa:borim/radiotray $BASE_DIR/radiotray*_source.changes
 done
-
-# update changelog for ppa
-# replace debian series with ubuntu series
-sed -i "1s/unstable;/$UBUNTU_SERIES;/" ppa/$PACKAGE_NAME/debian/changelog
-# update version
-sed -i "1s/)/-ppa$PPA_VERSION~$UBUNTU_SERIES)/" ppa/$PACKAGE_NAME/debian/changelog
-
-# create source package
-cd ppa/$PACKAGE_NAME
-debuild -S
-
-# upload source package
-cd ..
-dput ppa:borim/radiotray radiotray*_source.changes
 
