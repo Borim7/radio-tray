75cabef467ac7ab626977971ca78d21b56ead6d2
Matthias <borim7@web.de>
2021-12-28 13:47:04 +0100

    Add ubuntu releases for ppa upload

--- upload_ppa.sh
+++ upload_ppa.sh
@@ -3,7 +3,7 @@
 set -e
 
 # ppa require ubuntu distribution series in first line of changelog
-UBUNTU_SERIES="focal groovy"
+UBUNTU_SERIES="focal groovy hirsute impish"
 # only if same release need to be updated again
 PPA_VERSION=
 
