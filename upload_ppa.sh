25f7f9c8a975be61034bffa4388c1ed425a4f288
Matthias <borim7@web.de>
2022-05-27 17:23:24 +0200

    Add new ubuntu release: jammy

--- upload_ppa.sh
+++ upload_ppa.sh
@@ -3,7 +3,7 @@
 set -e
 
 # ppa require ubuntu distribution series in first line of changelog
-UBUNTU_SERIES="focal groovy hirsute impish"
+UBUNTU_SERIES="focal groovy hirsute impish jammy"
 # only if same release need to be updated again
 PPA_VERSION=
 
